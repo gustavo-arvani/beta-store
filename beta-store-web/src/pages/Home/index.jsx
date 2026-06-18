c1b4c413eaa7f31b9a700e1ad73b57d7ad614103
gustavoarvani2003-code <gustavo.arvani2003@gmail.com>
2026-06-18 00:30:57 -0300

    feat: integrar API de produtos na página inicial
    - Implementado consumo da API Fake Store
    - Adicionado carregamento dos produtos com Fetch API
    - Exibidos os primeiros produtos na página Home
    - Renderizadas as imagens dos produtos dinamicamente
    - Estruturada a listagem inicial para futuras melhorias de layout

--- beta-store-web/src/pages/Home/index.jsx
+++ beta-store-web/src/pages/Home/index.jsx
@@ -1,13 +1,40 @@
-import { Link } from 'react-router-dom';
-import './home.css';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
 
-function Home(){
-    return(
-        <div className='home'>
-            <h1> PÁGINA HOME </h1>
-            <Link to="./produto">PRODUTO</Link>
-        </div>
-    );
+function Home() {
+  const [store, setStore] = useState([]);
+
+  useEffect(() => {
+    function loadApi() {
+      let apiUrl = "https://fakestoreapi.com/Products";
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setStore(json);
+        });
+    }
+
+    loadApi();
+  }, []);
+
+  const firstItems = store.slice(0, 8);
+  return (
+    <div className="home">
+      <h2>O QUE VOCÊ PRECISA ESTÁ AQUI!</h2>
+
+      <div className="products">
+        {firstItems.map((item)=>{
+            return(
+                <div key={item.id} className="card">
+                    <img src={item.image} alt="" />
+                    </div>
+            )
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
